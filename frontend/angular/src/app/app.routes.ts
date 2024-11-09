0a00166348cf54b3b942ae552d8b614f1bfa91d8
venijamin <venijamintod@gmail.com>
2024-11-09 23:42:57 +0100

    [ADD] posts can now be created using form

--- frontend/angular/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import {AppComponent} from "./app.component";
-import {PostFormComponent} from "./posts/post-form/post-form.component";
-
-export const routes: Routes = [
-  { path: '/posts/create', component: PostFormComponent }, // Route for creating a post
-
-];
--- frontend/angular/src/app/model/post.ts
+++
@@ -1,9 +0,0 @@
-
-export interface Post {
-  post_id: string;
-  username: string;
-  title: string;
-  body: string;
-  up_vote: number;
-  down_vote: number;
-}

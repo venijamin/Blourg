7e8908c5ce447ef22734a185448831a22a2ae795
venijamin <venijamintod@gmail.com>
2025-02-10 20:04:49 +0100

    [ADD] post body is now smaller thanks to PostPreviewDTO reducing load

---
+++ model/Post/PostPreviewDTO.go
@@ -0,0 +1,10 @@
+package Post
+
+type PostPreviewDTO struct {
+	PostUUID string `json:"post_uuid" gorm:"primaryKey"`
+	UserUUID string `json:"user_uuid"`
+	Title    string `json:"title"`
+	Body     string `json:"body"`
+	UpVote   int    `json:"up_vote"`
+	DownVote int    `json:"down_vote"`
+}
